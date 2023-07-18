bd956b26edd02d1c8430c3118b19797870315ee7
Phil Henrickson <phil.henrickson@gmail.com>
2023-07-18 10:06:02 -0500

    configured _make_data target

--- jobs/run_make.R
+++ jobs/run_make.R
@@ -5,19 +5,19 @@ library(targets)
 # data --------------------------------------------------------------------
 
 # data pipeline
-my_script = "_make_data.R"
-
-# make data
+# set config to make yaml
 tar_config_set(script = "_make_data.R",
                store = "_make_data",
                config = "_make_data.yaml")
 
 # make
-tar_make(script = my_script)
+tar_make(script = "_make_data.R",
+         store = "_make_data")
 
 # viz
-tar_glimpse(script = my_script)
-tar_visnetwork(script = my_script)
+tar_glimpse(script = "_make_data.R")
+
+tar_visnetwork(script = "_make_data.R")
 
 
 # models ------------------------------------------------------------------
